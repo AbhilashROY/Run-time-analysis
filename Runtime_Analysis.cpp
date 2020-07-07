c74040b504d03bb40cb33690bbd44b2ad41dcd74
AbhilashROY <47791354+AbhilashROY@users.noreply.github.com>
2020-07-07 17:36:10 +0530

    Update Runtime_Analysis.cpp
    Formatting corrected

--- Runtime_Analysis.cpp
+++ Runtime_Analysis.cpp
@@ -13,8 +13,8 @@ string file_name;
 FILE* f;
 
 int test_karger_algorithm(vector<vector<int>>& g, int rep, int n)  //testing the Karger's Algorithm against a given graph 
-{																   //g is the graph; rep is the no. of times a single phase of
-	double start , end;											   //Karger's Algorithm will be repeated; n is no. of nodes in graph g
+{								   //g is the graph; rep is the no. of times a single phase of
+	double start , end;					   //Karger's Algorithm will be repeated; n is no. of nodes in graph g
 	start = get_time();  //Timer starts
 	int res = GetKargerMinCut(g,rep);  //Karger's Algorithm is called, the function is defined in 'Karger_Algorithm.h' header file
 	end = get_time();    //Timer ends
@@ -26,11 +26,11 @@ int test_karger_algorithm(vector<vector<int>>& g, int rep, int n)  //testing the
 }
 
 int test_stoer_wagner_algorithm(vector<vector<int>>& g, int n)  //testing the Stoer-Wagner Algorithm against a given graph 
-{																//g is the graph; n is no. of nodes in graph g
+{								//g is the graph; n is no. of nodes in graph g
 	double start , end;										
 	start = get_time();
 	int res = GetStoerWagnerMinCut(g);
-	end = get_time();											//All parameters here are similar to the above function
+	end = get_time();				        //All parameters here are similar to the above function
 	double runtime = end - start;
 	fprintf(f, "%d ", n);
 	fprintf(f, "%f\n", runtime);
@@ -39,7 +39,7 @@ int test_stoer_wagner_algorithm(vector<vector<int>>& g, int n)  //testing the St
 }
 
 void test_karger_algorithm_driver()   //Driver function for Karger's algorithm which selects the different files 
-{									  //to which runtime data will be written to and the type of graph for a particular number of nodes n
+{				      //to which runtime data will be written to and the type of graph for a particular number of nodes n
 	file_name="runtime_data/data_sparse_karger.dat"; //Path of the file to which data will be written
 	f = fopen(file_name.c_str(),"w");                //Opening the partucular file
 	for(int n = 2; n < 40; n+=2)                     //Iterating through graphs of different sizes
@@ -65,7 +65,7 @@ void test_karger_algorithm_driver()   //Driver function for Karger's algorithm w
 	fclose(f);
 
 	file_name="runtime_data/data_very_dense_karger.dat";
-	f = fopen(file_name.c_str(),"w");						  //Similar to above function
+	f = fopen(file_name.c_str(),"w");			//Similar to above function
 	for(int n = 2; n < 40; n+=2)
 	{
 		cout<<"For n = "<<n<<" : "<<endl;
@@ -78,7 +78,7 @@ void test_karger_algorithm_driver()   //Driver function for Karger's algorithm w
 }
 
 void test_stoer_wagner_algorithm_driver()  //Driver function for Stoer-Wagner algorithm which selects the different files 
-{										   //to which runtime data will be written to and the type of graph for a particular number of nodes n
+{					   //to which runtime data will be written to and the type of graph for a particular number of nodes n
 	file_name="runtime_data/data_sparse_stoer_wagner.dat";
 	f = fopen(file_name.c_str(),"w");                          //Similar to above function
 	for(int n = 2; n < 300; n+=5)
