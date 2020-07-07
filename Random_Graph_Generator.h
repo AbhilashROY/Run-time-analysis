e0d2c596fbc4117404c7db86319f87bab29289c5
AbhilashROY <47791354+AbhilashROY@users.noreply.github.com>
2020-07-07 17:33:51 +0530

    Update Random_Graph_Generator.h
    Formatting corrected

--- Random_Graph_Generator.h
+++ Random_Graph_Generator.h
@@ -11,7 +11,7 @@ int fin(int x)
 {
 	if(ln[x]==x) return x;
 	return ln[x]=fin(ln[x]);				//DSU is used here to ensure that the graphs 
-}											//that this random generator generates is
+}								//that this random generator generates is
 void unite(int x, int y)					//always connected.
 {
 	int a=fin(x),b=fin(y);
@@ -26,10 +26,10 @@ bool same(int x, int y)
 	if(fin(x)==fin(y)) return true;
 	return false;
 }
-									     //Disjoint Set Union(DSU) implementation ends
+					   //Disjoint Set Union(DSU) implementation ends
 
 vector<vector<int> > generate_sparse_list(int n)  //Function that returns a adjacency list of a random sparse graph 
-{												  //with n nodes
+{						  //with n nodes
 	DSUinit();
 	vector<vector<int> > g(n+1);
 	vector<pair<int,int> > edges;
@@ -58,7 +58,7 @@ vector<vector<int> > generate_sparse_list(int n)  //Function that returns a adja
 }
 
 vector<vector<int> > generate_dense_list(int n)  //Function that returns a adjacency list of a random dense graph 
-{												 //with n nodes
+{					         //with n nodes
 	DSUinit();
 	vector<vector<int> > g(n+1);
 	vector<pair<int,int> > edges;
@@ -98,7 +98,7 @@ vector<vector<int> > generate_dense_list(int n)  //Function that returns a adjac
 }
 
 vector<vector<int> > generate_very_dense_list(int n)  //Function that returns a adjacency list of a random very dense graph 
-{												      //with n nodes
+{						      //with n nodes
 	vector<vector<int> > g(n+1); 
 	for(int i=1;i<=n-1;i++)
 	{
@@ -112,7 +112,7 @@ vector<vector<int> > generate_very_dense_list(int n)  //Function that returns a 
 }
 
 vector<vector<int> > generate_sparse_matrix(int n)  //Function that returns a adjacency matrix of a random sparse graph 
-{													//with n nodes
+{						    //with n nodes
 	vector<vector<int> > t(n+1);
 	vector<vector<int> > g(n+1,vector<int>(n+1,0) );
 	t=generate_sparse_list(n);
@@ -128,7 +128,7 @@ vector<vector<int> > generate_sparse_matrix(int n)  //Function that returns a ad
 }
 
 vector<vector<int> > generate_dense_matrix(int n)  //Function that returns a adjacency matrix of a random dense graph
-{												   //with n nodes
+{					           //with n nodes
 	vector<vector<int> > t(n+1);
 	vector<vector<int> > g(n+1,vector<int>(n+1,0) );
 	t=generate_dense_list(n);
