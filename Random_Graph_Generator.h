c1a9fec3ebbb03ff112d0faf99a9dd38f1d16e42
AbhilashROY <47791354+AbhilashROY@users.noreply.github.com>
2020-07-07 17:30:09 +0530

    Create Runtime_Analysis.cpp

---
+++ Random_Graph_Generator.h
@@ -0,0 +1,155 @@
+const int N=1e3;                          //Disjoint Set Union(DSU) implementation starts
+vector<int> ln(N+2),rn(N+2);
+
+void DSUinit()
+{
+	for(int i=0;i<=N;i++)
+		ln[i]=i,rn[i]=1;
+}
+
+int fin(int x)
+{
+	if(ln[x]==x) return x;
+	return ln[x]=fin(ln[x]);				//DSU is used here to ensure that the graphs 
+}											//that this random generator generates is
+void unite(int x, int y)					//always connected.
+{
+	int a=fin(x),b=fin(y);
+	if(a!=b)
+	{
+		if(rn[a]<rn[b]) swap(a,b);
+		ln[b]=a,rn[a]+=rn[b];
+	}
+}
+bool same(int x, int y)
+{
+	if(fin(x)==fin(y)) return true;
+	return false;
+}
+									     //Disjoint Set Union(DSU) implementation ends
+
+vector<vector<int> > generate_sparse_list(int n)  //Function that returns a adjacency list of a random sparse graph 
+{												  //with n nodes
+	DSUinit();
+	vector<vector<int> > g(n+1);
+	vector<pair<int,int> > edges;
+	for(int i=1;i<=n-1;i++)
+	{
+		for(int j=i+1;j<=n;j++)
+		{
+			edges.push_back({i,j});
+		}
+	}
+	while(1)
+	{
+		int s=edges.size();
+		int a=rand()%s;
+		int x=edges[a].first,y=edges[a].second;
+		if(!same(x,y))
+		{
+			unite(x,y);
+			g[x].push_back(y);
+			g[y].push_back(x);
+			edges.erase(edges.begin()+a);
+			if(rn[fin(x)]>=n) break;
+		}
+	}
+	return g;
+}
+
+vector<vector<int> > generate_dense_list(int n)  //Function that returns a adjacency list of a random dense graph 
+{												 //with n nodes
+	DSUinit();
+	vector<vector<int> > g(n+1);
+	vector<pair<int,int> > edges;
+	for(int i=1;i<=n-1;i++)
+	{
+		for(int j=i+1;j<=n;j++)
+		{
+			edges.push_back({i,j});
+		}
+	}
+	while(1)
+	{
+		int s=edges.size();
+		int a=rand()%s;
+		int x=edges[a].first,y=edges[a].second;
+		if(!same(x,y))
+		{
+			unite(x,y);
+			g[x].push_back(y);
+			g[y].push_back(x);
+			edges.erase(edges.begin()+a);
+			if(rn[fin(x)]>=n) break;
+		}
+	}
+	int e=(n*(n-1))/6-(n-1);
+	while(e>0)
+	{
+		int s=edges.size();
+		int a=rand()%s;
+		int x=edges[a].first,y=edges[a].second;
+		g[x].push_back(y);
+		g[y].push_back(x);
+		edges.erase(edges.begin()+a);
+		e--;
+	}
+	return g;
+}
+
+vector<vector<int> > generate_very_dense_list(int n)  //Function that returns a adjacency list of a random very dense graph 
+{												      //with n nodes
+	vector<vector<int> > g(n+1); 
+	for(int i=1;i<=n-1;i++)
+	{
+		for(int j=i+1;j<=n;j++)
+		{
+			g[i].push_back(j);
+			g[j].push_back(i);
+		}
+	}
+	return g;
+}
+
+vector<vector<int> > generate_sparse_matrix(int n)  //Function that returns a adjacency matrix of a random sparse graph 
+{													//with n nodes
+	vector<vector<int> > t(n+1);
+	vector<vector<int> > g(n+1,vector<int>(n+1,0) );
+	t=generate_sparse_list(n);
+	for(int i=1;i<=n;i++)
+	{
+		for(auto x: t[i])
+		{
+			if(g[i][x]==g[x][i]&&g[i][x]==0)
+				g[i][x]=g[x][i]=(int)rand()%20;
+		}
+	}
+	return g;
+}
+
+vector<vector<int> > generate_dense_matrix(int n)  //Function that returns a adjacency matrix of a random dense graph
+{												   //with n nodes
+	vector<vector<int> > t(n+1);
+	vector<vector<int> > g(n+1,vector<int>(n+1,0) );
+	t=generate_dense_list(n);
+	for(int i=1;i<=n;i++)
+	{
+		for(auto x: t[i])
+		{
+			if(g[i][x]==g[x][i]&&g[i][x]==0)
+				g[i][x]=g[x][i]=(int)rand()%20;
+		}
+	}
+	return g;
+}
+
+vector<vector<int> > generate_very_dense_matrix(int n)  //Function that returns a adjacency matrix of a random very dense graph
+{														//with n nodes
+	vector<vector<int> > g(n+1,vector<int>(n+1,0) );
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i+1;j<=n;j++) g[i][j]=g[j][i]=(int)rand()%20;
+	}
+	return g;
+}
+
---
+++ Runtime_Analysis.cpp
@@ -0,0 +1,134 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include"Karger_Algorithm.h"
+#include"Stoer_Wagner_Algorithm.h"
+#include"Random_Graph_Generator.h"
+
+double get_time()
+{
+	return (double)clock()/CLOCKS_PER_SEC;  //Returns the time in seconds
+}
+
+string file_name;
+FILE* f;
+
+int test_karger_algorithm(vector<vector<int>>& g, int rep, int n)  //testing the Karger's Algorithm against a given graph 
+{																   //g is the graph; rep is the no. of times a single phase of
+	double start , end;											   //Karger's Algorithm will be repeated; n is no. of nodes in graph g
+	start = get_time();  //Timer starts
+	int res = GetKargerMinCut(g,rep);  //Karger's Algorithm is called, the function is defined in 'Karger_Algorithm.h' header file
+	end = get_time();    //Timer ends
+	double runtime = end - start;  //Calculates the time required to run the algorithm
+	fprintf(f, "%d ", n); //Writes the data to file
+	fprintf(f, "%f\n", runtime);  //Writes the data to file
+	cout << "Runtime is " << runtime << endl;  //Outputs the runtime in console
+	return res; // Returns the result
+}
+
+int test_stoer_wagner_algorithm(vector<vector<int>>& g, int n)  //testing the Stoer-Wagner Algorithm against a given graph 
+{																//g is the graph; n is no. of nodes in graph g
+	double start , end;										
+	start = get_time();
+	int res = GetStoerWagnerMinCut(g);
+	end = get_time();											//All parameters here are similar to the above function
+	double runtime = end - start;
+	fprintf(f, "%d ", n);
+	fprintf(f, "%f\n", runtime);
+	cout << "Runtime is " << runtime << endl;
+	return res;
+}
+
+void test_karger_algorithm_driver()   //Driver function for Karger's algorithm which selects the different files 
+{									  //to which runtime data will be written to and the type of graph for a particular number of nodes n
+	file_name="runtime_data/data_sparse_karger.dat"; //Path of the file to which data will be written
+	f = fopen(file_name.c_str(),"w");                //Opening the partucular file
+	for(int n = 2; n < 40; n+=2)                     //Iterating through graphs of different sizes
+	{
+		cout<<"For n = "<<n<<" : "<<endl;
+		int rep = n*n*log2(n); //Specifies the number of iterations for the Karger's algorithm
+		vector<vector<int>> g = generate_sparse_list(n); //Generates a sparse connected graph with n nodes
+		int res = test_karger_algorithm(g, rep, n); //Calls the testing function
+		cout << "The min-cut is : " << res << "\n\n"; //Outputs the result, i.e, the min-cut of the graph
+	}
+	fclose(f); // file is closed
+
+	file_name="runtime_data/data_dense_karger.dat";
+	f = fopen(file_name.c_str(),"w");                         //Similar to above function
+	for(int n = 2; n < 40; n+=2)
+	{
+		cout<<"For n = "<<n<<" : "<<endl;
+		int rep = n*n*log2(n);
+		vector<vector<int>> g = generate_dense_list(n);//Generates a dense connected graph with n nodes
+		int res = test_karger_algorithm(g, rep, n);
+		cout << "The min-cut is : " << res << "\n\n";
+	}
+	fclose(f);
+
+	file_name="runtime_data/data_very_dense_karger.dat";
+	f = fopen(file_name.c_str(),"w");						  //Similar to above function
+	for(int n = 2; n < 40; n+=2)
+	{
+		cout<<"For n = "<<n<<" : "<<endl;
+		int rep = n*n*log2(n);
+		vector<vector<int>> g = generate_very_dense_list(n);//Generates a very dense connected graph with n nodes
+		int res = test_karger_algorithm(g, rep, n);
+		cout << "The min-cut is : " << res << "\n\n";
+	}
+	fclose(f);
+}
+
+void test_stoer_wagner_algorithm_driver()  //Driver function for Stoer-Wagner algorithm which selects the different files 
+{										   //to which runtime data will be written to and the type of graph for a particular number of nodes n
+	file_name="runtime_data/data_sparse_stoer_wagner.dat";
+	f = fopen(file_name.c_str(),"w");                          //Similar to above function
+	for(int n = 2; n < 300; n+=5)
+	{
+		cout<<"For n = "<<n<<" : "<<endl;
+		vector<vector<int>> g = generate_sparse_matrix(n);
+		int res = test_stoer_wagner_algorithm(g, n);
+		cout << "The min-cut is : " << res << "\n\n";
+	}
+	fclose(f);
+
+	file_name="runtime_data/data_dense_stoer_wagner.dat";
+	f = fopen(file_name.c_str(),"w");                          //Similar to above function
+	for(int n = 2; n < 300; n+=5)
+	{
+		cout<<"For n = "<<n<<" : "<<endl;
+		vector<vector<int>> g = generate_dense_matrix(n);
+		int res = test_stoer_wagner_algorithm(g, n);
+		cout << "The min-cut is : " << res << "\n\n";
+	}
+	fclose(f);
+
+	file_name="runtime_data/data_very_dense_stoer_wagner.dat";
+	f = fopen(file_name.c_str(),"w");                          //Similar to above function
+	for(int n = 2; n < 300; n+=5)
+	{
+		cout<<"For n = "<<n<<" : "<<endl;
+		vector<vector<int>> g = generate_very_dense_matrix(n);
+		int res = test_stoer_wagner_algorithm(g, n);
+		cout << "The min-cut is : " << res << "\n\n";
+	}
+	fclose(f);
+}
+
+int main()
+{
+	test_karger_algorithm_driver();           //Calls the driver function for Karger's Algorithm
+	test_stoer_wagner_algorithm_driver();     //Calls the driver function for Stoer-Wagner Algorithm
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
